446e0183216bfb7c8560c8ca94df7d27f5f5c3de
Artur Zainutdinov <zainutdinovartur9@gmail.com>
2024-01-10 23:33:57 +0300

    done

---
+++ src/day00_ex02.sql
@@ -0,0 +1,13 @@
+SELECT name, rating
+FROM pizzeria
+WHERE pizzeria.rating >= 3.5 AND pizzeria.rating <= 5
+ORDER BY 2;
+
+
+
+SELECT name, rating
+FROM pizzeria
+WHERE pizzeria.rating BETWEEN 3.5 AND 5
+ORDER BY 2;
+
+
